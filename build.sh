f5bdf25bd8b22e8162fc5d18a5f41f095c4e6c64
Jens Axel Søgaard <jensaxel@soegaard.net>
2012-07-19 16:57:15 +0200

    build.sh now uses insert-mathjax.rkt

--- build.sh
+++ build.sh
@@ -1,7 +1,14 @@
+echo "Copying triangle-solver.rkt to build/"
 cp triangle-solver.rkt build/
+echo "Running Whalesong on triangle-solver.rkt"
 cd build
 ../../new-whalesong/whalesong/whalesong.rkt build triangle-solver.rkt
-#sed -i '.tmp' '/<\/script>/ r mathjax-script.js' triangle-solver.html
 cd ..
-# TODO
-# Now insert content of mathjax-script.js in build/triangle-solver.rkt
+echo "Injecting MathJax into triangle-solver.html"
+racket -t insert-mathjax.rkt
+mv build/triangle-solver.html.tmp build/triangle-solver.html
+echo "Done. Use:"
+echo "open build/triangle-solver.html"
+
+
+
