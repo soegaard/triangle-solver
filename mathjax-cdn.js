7e0a455cdc8240a1b6164ad5e8d54af729552c20
Jens Axel Søgaard <jensaxel@soegaard.net>
2012-07-19 16:58:24 +0200

    Added cdn version of mathjax

---
+++ mathjax-cdn.js
@@ -0,0 +1,25 @@
+<script type="text/javascript" src="http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML">
+  MathJax.Hub.Config({
+    extensions: ["tex2jax.js"],
+    jax: ["input/TeX", "output/HTML-CSS"],
+    tex2jax: {
+      inlineMath: [ ['$','$'], ["\\(","\\)"] ],
+      displayMath: [ ['$$','$$'], ["\\[","\\]"] ],
+      processEscapes: true
+    },
+    "HTML-CSS": { availableFonts: ["TeX"] }
+  });
+</script>
+<style type="text/css">.MathJax_Preview {color: #888}
+#MathJax_Message {position: fixed; left: 1em; bottom: 1.5em; background-color: #E6E6E6; border: 1px solid #959595; margin: 0px; padding: 2px 8px; z-index: 102; color: black; font-size: 80%; width: auto; white-space: nowrap}
+#MathJax_MSIE_Frame {position: absolute; top: 0; left: 0; width: 0px; z-index: 101; border: 0px; margin: 0px; padding: 0px}
+.MathJax_Error {color: #CC0000; font-style: italic}
+</style>
+<script>
+var UpdateMath = function () {
+                     MathJax.Hub.Queue(["Typeset",MathJax.Hub]); 
+                     window.setTimeout(UpdateMath, 1000*1);                                      
+                     return true;
+                 };
+window.setTimeout(UpdateMath, 1000*1);
+</script>
