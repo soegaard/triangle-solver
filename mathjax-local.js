42894e50ebbd47642e61d81c0cfed7586a17063c
Jens Axel Søgaard <jensaxel@soegaard.net>
2012-07-19 16:58:47 +0200

    Added local version of mathjax script

--- mathjax-local.js
+++ mathjax-local.js
@@ -1,4 +1,4 @@
-<script type="text/javascript" src="MathJax/MathJax.js">
+<script type="text/javascript" src="mathjax/MathJax.js">
   MathJax.Hub.Config({
     extensions: ["tex2jax.js"],
     jax: ["input/TeX", "output/HTML-CSS"],
